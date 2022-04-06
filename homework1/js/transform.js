597ca2ce9f85cabdcda941e02918c45283710bf2
MEL <melguo@stanford.edu>
2022-04-05 21:02:03 -0700

    Commented out recent code as a test

--- homework1/js/transform.js
+++ homework1/js/transform.js
@@ -47,9 +47,11 @@ var MVPmat = function ( dispParams ) {
 		var translationMatrix = new THREE.Matrix4().makeTranslation(state.modelTranslation.x, 
 			state.modelTranslation.y, state.modelTranslation.z);
 		var rotationMatrix = new THREE.Matrix4();
-		rotationMatrix.makeRotationX(state.modelRotation.y * (Math.PI/180));
-		rotationMatrix.makeRotationY(state.modelRotation.x * (Math.PI/180));
-		return rotationMatrix * translationMatrix;
+		//rotationMatrix.makeRotationX(state.modelRotation.y * (Math.PI/180));
+		//rotationMatrix.makeRotationY(state.modelRotation.x * (Math.PI/180));
+		//return rotationMatrix * translationMatrix;
+		return translationMatrix;
+	
 
 		/* TODO (2.1.1.3) Matrix Update / (2.1.2) Model Rotation  */
 
