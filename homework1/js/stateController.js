4f4dd0ae5cbbc209032227db044fb9d6af3ebb70
MEL <melguo@stanford.edu>
2022-04-05 20:04:38 -0700

    Changed calculations in computeMovement in stateController.js

--- homework1/js/stateController.js
+++ homework1/js/stateController.js
@@ -160,8 +160,8 @@ var StateController = function ( dispParams ) {
 	function computeMovement( x, y, previousPosition ) {
 
 		/* TODO (2.1.1.1) Mouse Movement */
-		var x_movement = x - previousPosition.x;
-		var y_movement = y - previousPosition.y;
+		var x_movement = previousPosition.x - x;
+		var y_movement = previousPosition.y - y;
 		var movement = new THREE.Vector2(x_movement, y_movement);
 		previousPosition.x = x;
 		previousPosition.y = y;
